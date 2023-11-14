36608bc1a381480cfb628dd27601b87ad7b13c97
brd3v <bnsiq2015@gmail.com>
2023-11-14 00:46:20 -0300

    trabalhandoo com numeeros parte um

---
+++ dadoos_numericos.py
@@ -0,0 +1,19 @@
+num1 = 5 
+num2 = 3.6
+num_string = '5'
+
+print(type(num_string))
+
+a = float(5)
+b = float(a)
+print(b)
+print(a)
+print(type(b))
+
+b = int(num2)
+print(b)
+print(type(b))
+
+c = int(num_string)
+print(c)
+print(type(c))
