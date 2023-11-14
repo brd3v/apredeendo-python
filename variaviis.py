b4c2dbdd51a042573233137fd0fc6756309cf98a
brd3v <bnsiq2015@gmail.com>
2023-11-14 00:36:43 -0300

    apredendo pythoon

---
+++ variaviis.py
@@ -0,0 +1,11 @@
+nome = 'Bruno'
+idade = '25'
+idade = 26
+eh_bom_de_ti = True
+gatos = ['lili', 'zetsu', 'hering', 'belga']
+gosto = 'programação'
+print(f'Meeu nome é {nome }e trabalho com marketing digital')
+print(f'eu gosto de {gosto}')
+print(f'eu tenho tenho {idade} anos')
+print(f'gosta de ti {eh_bom_de_ti}')
+print(gatos)
